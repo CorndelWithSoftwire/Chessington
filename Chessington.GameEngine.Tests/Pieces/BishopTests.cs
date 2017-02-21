c093189b3bf7a34dde3425b8b1bef23427575888
Matthew Richards <Matthew.Richards@softwire.com>
2017-02-21 13:49:47 +0000

    Added Unit Tests for Bishop movement

--- Chessington.GameEngine.Tests/Pieces/BishopTests.cs
+++ Chessington.GameEngine.Tests/Pieces/BishopTests.cs
@@ -1,9 +1,36 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using Chessington.GameEngine.Pieces;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace Chessington.GameEngine.Tests.Pieces
 {
     [TestFixture]
     public class BishopTests
-    { 
+    {
+        [Test]
+        public void Bishop_CanMove_Diagonally()
+        {
+            var board = new Board();
+            var bishop = new Bishop(Player.White);
+            board.AddPiece(Square.At(4, 4), bishop);
+
+            var moves = bishop.GetAvailableMoves(board);
+
+            var expectedMoves = new List<Square>();
+
+            //Checking the backwards diagonal, i.e. 0,0 1,1, 2,2
+            for (var i = 0; i < 8; i++)
+                expectedMoves.Add(Square.At(i, i));
+
+            //Checking the forwards diagonal i.e. 5,3 6,2 7,1
+            for (var i = 1; i < 8; i++)
+                expectedMoves.Add(Square.At(i, 8 - i));
+
+            //Get rid of our starting location.
+            expectedMoves.RemoveAll(s => s == Square.At(4, 4));
+
+            moves.ShouldAllBeEquivalentTo(expectedMoves);
+        }
     }
 }
