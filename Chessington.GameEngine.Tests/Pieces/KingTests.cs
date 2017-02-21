8b09ab4e9be1921cb02d0f4d61f7dadc4e08d493
Matthew Richards <Matthew.Richards@softwire.com>
2017-02-21 13:52:04 +0000

    Added Unit Tests for King movement

--- Chessington.GameEngine.Tests/Pieces/KingTests.cs
+++ Chessington.GameEngine.Tests/Pieces/KingTests.cs
@@ -1,9 +1,35 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using Chessington.GameEngine.Pieces;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace Chessington.GameEngine.Tests.Pieces
 {
     [TestFixture]
     public class KingTests
     {
+        [Test]
+        public void KingsCanMoveToAdjacentSquares()
+        {
+            var board = new Board();
+            var king = new King(Player.White);
+            board.AddPiece(Square.At(4, 4), king);
+
+            var moves = king.GetAvailableMoves(board);
+
+            var expectedMoves = new List<Square>
+            {
+                Square.At(3, 3),
+                Square.At(3, 4),
+                Square.At(3, 5),
+                Square.At(4, 3),
+                Square.At(4, 5),
+                Square.At(5, 3),
+                Square.At(5, 4),
+                Square.At(5, 5)
+            };
+
+            moves.ShouldAllBeEquivalentTo(expectedMoves);
+        }
     }
 }
