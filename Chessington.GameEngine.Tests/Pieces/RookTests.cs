ebc6248570afd48aac01e2c0eb13c74c9c5af790
Matthew Richards <Matthew.Richards@softwire.com>
2017-02-21 13:48:56 +0000

    Added Unit Tests for Rook movement

--- Chessington.GameEngine.Tests/Pieces/RookTests.cs
+++ Chessington.GameEngine.Tests/Pieces/RookTests.cs
@@ -1,9 +1,33 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using Chessington.GameEngine.Pieces;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace Chessington.GameEngine.Tests.Pieces
 {
     [TestFixture]
     public class RookTests
     {
+        [Test]
+        public void Rook_CanMove_Laterally()
+        {
+            var board = new Board();
+            var rook = new Rook(Player.White);
+            board.AddPiece(Square.At(4, 4), rook);
+
+            var moves = rook.GetAvailableMoves(board);
+            var expectedMoves = new List<Square>();
+
+            for (var i = 0; i < 8; i++)
+                expectedMoves.Add(Square.At(4, i));
+
+            for (var i = 0; i < 8; i++)
+                expectedMoves.Add(Square.At(i, 4));
+
+            //Get rid of our starting location.
+            expectedMoves.RemoveAll(s => s == Square.At(4, 4));
+
+            moves.Should().Contain(expectedMoves);
+        }
     }
 }
