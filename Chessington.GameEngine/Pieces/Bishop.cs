e26f9e4078e908e112a20edccb150ab63a1d8704
Michael Collingwood <mike@fairways.me>
2020-07-13 11:40:18 +0100

    bishop basic move potentials

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,7 +10,27 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var availableMoves = new List<Square>();
+            for (int i = -GameSettings.BoardSize; i < GameSettings.BoardSize; i++)
+            {
+                if ((board.FindPiece(this).Row + i <= GameSettings.BoardSize) && (board.FindPiece(this).Col + i <= GameSettings.BoardSize))
+                {
+                    availableMoves.Add(Square.At(board.FindPiece(this).Row + i, board.FindPiece(this).Col + i));
+                }
+                else if ((board.FindPiece(this).Row + i <= GameSettings.BoardSize) && (board.FindPiece(this).Col - i >= 0))
+                {
+                    availableMoves.Add(Square.At(board.FindPiece(this).Row + i, board.FindPiece(this).Col - i));
+                }
+            }
+
+            return availableMoves;
+        }
+
+        private bool CheckPosition(int row, int col)
+        {
+            return (row <= GameSettings.BoardSize && row >= 0 && col <= GameSettings.BoardSize && col >= 0
+                ? true
+                : false);
         }
     }
 }
