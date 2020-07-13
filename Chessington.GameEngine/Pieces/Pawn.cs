e70ce63e937a7163b3f7f2c678ac6dd5b4c8ad77
Michael Collingwood <mike@fairways.me>
2020-07-13 10:32:24 +0100

    resolve pawn test errors

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -5,12 +5,23 @@ namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
-        public Pawn(Player player) 
-            : base(player) { }
+        public Pawn(Player player)
+            : base(player)
+        {
+            
+        }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            return new Square[]
+            {
+                Square.At(Operation(board.FindPiece(this).Row,1), board.FindPiece(this).Col)
+            };
+        }
+
+        private int Operation(int number1, int number2)
+        {
+            return number1 + (Player == Player.Black ? 1 : -1) * number2;
         }
     }
 }
