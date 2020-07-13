f082166962d513230e3f41dcc9aef2bdaf2db9a1
emilies03 <Shelly7xxx>
2020-07-13 10:59:32 +0100

    Bishop moving diagonally

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,7 +10,28 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> availableMoves = new List<Square>();
+            Square here = board.FindPiece(this);
+            foreach (var index in Enumerable.Range(1,7))
+            {
+                if (index<=here.Col && index<=here.Row)
+                {
+                    availableMoves.Add(new Square(here.Row-index, here.Col-index));
+                }
+                if (index<=here.Col && index<8-here.Row)
+                {
+                    availableMoves.Add(new Square(here.Row+index, here.Col-index));
+                }
+                if (index<8-here.Col && index<8-here.Row)
+                {
+                    availableMoves.Add(new Square(here.Row+index, here.Col+index));
+                }
+                if (index<8-here.Col && index<=here.Row)
+                {
+                    availableMoves.Add(new Square(here.Row-index, here.Col+index));
+                }
+            }
+            return availableMoves;
         }
     }
 }
