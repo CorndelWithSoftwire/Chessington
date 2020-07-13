4be2d2aeda9e19490981e4049f92b4aa5f9b6352
Hizkia Susanto <hizkia.susanto@st-hughs.ox.ac.uk>
2020-07-13 18:13:38 +0700

    Implement King moves

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -6,11 +6,32 @@ namespace Chessington.GameEngine.Pieces
     public class King : Piece
     {
         public King(Player player)
-            : base(player) { }
+            : base(player)
+        {
+        }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentPos = board.FindPiece(this);
+            var currentRow = currentPos.Row;
+            var currentCol = currentPos.Col;
+            List<Square> availableMoves = new List<Square>();
+
+            foreach (var rowDiff in Enumerable.Range(-1, 3))
+            {
+                foreach (var colDiff in Enumerable.Range(-1, 3))
+                {
+                    if (Square.At(currentRow + rowDiff, currentCol + colDiff).IsInbound())
+                    {
+                        availableMoves.Add(Square.At(currentRow + rowDiff, currentCol + colDiff));
+                    }
+                }
+            }
+
+            availableMoves.RemoveAll(x => x == currentPos);
+
+            availableMoves.OrderBy(x => x.Row).OrderBy(x => x.Col);
+            return availableMoves;
         }
     }
 }
