07fa113e385ed74a60e37cad3eabb81608191be5
Whiles9799 <t.while@live.co.uk>
2020-07-13 16:46:43 +0100

    Implements pawns taking opposing pieces

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -6,42 +6,55 @@ namespace Chessington.GameEngine.Pieces
     public class Pawn : Piece
     {
         
-        
         public Pawn(Player player) 
             : base(player) { }
 
+        public IEnumerable<Square> PawnMove(Board board, int direction, int startingRow)
+        {
+            var currentPos = board.FindPiece(this);
+            var currentRow = currentPos.Row;
+            var currentCol = currentPos.Col;
+
+            if (Square.At(currentRow + direction, currentCol).CanMoveTo(board))
+            {
+                yield return Square.At(currentRow + direction, currentCol);
+
+                if (currentRow == startingRow &&
+                    Square.At(currentRow + 2 * direction, currentCol).CanMoveTo(board))
+                {
+                    yield return Square.At(currentRow + 2 * direction, currentCol);
+                }
+            }
+        }
+
+        public IEnumerable<Square> PawnTake(Board board, int direction)
+        {
+            var currentPos = board.FindPiece(this);
+            var currentRow = currentPos.Row;
+            var currentCol = currentPos.Col;
+
+            var diagonallyRight = Square.At(currentRow + direction, currentCol + 1);
+            var diagonallyLeft = Square.At(currentRow + direction, currentCol - 1);
+            if (diagonallyRight.CanMoveOrTake(board, this) && diagonallyRight.IsOccupied(board))
+            {
+                yield return Square.At(currentRow + direction, currentCol + 1);
+            }
+            if (diagonallyLeft.CanMoveOrTake(board, this) && diagonallyLeft.IsOccupied(board))
+            {
+                yield return Square.At(currentRow + direction, currentCol - 1);
+            }
+        }
+        
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            var currentPosition = board.FindPiece(this);
-            List<Square> availableMoves = new List<Square>();
-            
             if (Player == Player.White)
             {
-                if (Square.At(currentPosition.Row - 1, currentPosition.Col).CanMoveTo(board))
-                {
-                    availableMoves.Add(new Square(currentPosition.Row - 1, currentPosition.Col));
-
-                    if (currentPosition.Row == 6 &&
-                        !Square.At(currentPosition.Row - 2, currentPosition.Col).IsOccupied(board))
-                    {
-                        availableMoves.Add(new Square(currentPosition.Row - 2, currentPosition.Col));
-                    }
-                }
+                return PawnMove(board, -1, GameSettings.BoardSize - 2).Concat(PawnTake(board, -1));
             }
             else
             {
-                if (Square.At(currentPosition.Row + 1, currentPosition.Col).CanMoveTo(board))
-                {
-                    availableMoves.Add(new Square(currentPosition.Row + 1, currentPosition.Col));
-
-                    if (currentPosition.Row == 1 &&
-                        !Square.At(currentPosition.Row + 2, currentPosition.Col).IsOccupied(board))
-                    {
-                        availableMoves.Add(new Square(currentPosition.Row + 2, currentPosition.Col));
-                    }
-                }
+                return PawnMove(board, 1, 1).Concat(PawnTake(board, 1));
             }
-            return availableMoves;
         }
     }
 }
