54666e3473cbc04f2340be5aa18af1f537af942e
DougBoyle <dab80@cam.ac.uk>
2020-07-13 11:59:00 +0100

    Remove magic 8

--- Chessington.GameEngine/Pieces/Moves.cs
+++ Chessington.GameEngine/Pieces/Moves.cs
@@ -6,7 +6,7 @@ namespace Chessington.GameEngine.Pieces {
         public static IEnumerable<Square> GetLateralMoves(Board board, Square currentPosition)
         {
             List<Square> availableMoves = new List<Square>();
-            foreach (var index in Enumerable.Range(0,8))
+            foreach (var index in Enumerable.Range(0,GameSettings.BoardSize))
             {
                 if (currentPosition.Col != index)
                 {
@@ -29,15 +29,17 @@ namespace Chessington.GameEngine.Pieces {
             {
                 availableMoves.Add(new Square(currentPosition.Row-index, currentPosition.Col-index));
             }
-            for (var index=1;index<=currentPosition.Col && index<8-currentPosition.Row;index ++)
+            for (var index=1;index<=currentPosition.Col && index<GameSettings.BoardSize-currentPosition.Row;index ++)
             {
                 availableMoves.Add(new Square(currentPosition.Row+index, currentPosition.Col-index));
             }
-            for (var index=1;index<8-currentPosition.Col && index<8-currentPosition.Row;index ++)
+            for (var index=1;
+                index<GameSettings.BoardSize-currentPosition.Col && index<GameSettings.BoardSize-currentPosition.Row;
+                index ++)
             {
                 availableMoves.Add(new Square(currentPosition.Row+index, currentPosition.Col+index));
             }
-            for (var index=1;index<8-currentPosition.Col && index<=currentPosition.Row; index++)
+            for (var index=1;index<GameSettings.BoardSize-currentPosition.Col && index<=currentPosition.Row; index++)
             {
                 availableMoves.Add(new Square(currentPosition.Row - index, currentPosition.Col + index));
             }
