db35a30736f1d8f4caffdbf35a29163e5d96ca5a
Benji Marshall <Benji.Marshall@softwire.com>
2020-10-16 14:49:39 +0100

    Added Queen movement

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -10,7 +10,10 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var lateralMoves = GetAvailableLateralMoves(board);
+            var diagonalMoves = GetAvailableDiagonalMoves(board);
+
+            return lateralMoves.Union(diagonalMoves);
         }
     }
 }
