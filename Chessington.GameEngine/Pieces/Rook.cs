a0d845d206b4111cf118356eabc7fecec6f5af8d
Shivam <Shivam.Chandarana@softwire.com>
2020-10-16 14:41:30 +0100

    Test Red-03 Complete

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Win32.SafeHandles;
 
 namespace Chessington.GameEngine.Pieces
 {
@@ -10,7 +11,17 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var moves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+            for (int i = 1; i < GameSettings.BoardSize; i++)
+            {
+                var row = (currentSquare.Row + i) % GameSettings.BoardSize;
+                var col = (currentSquare.Col + i) % GameSettings.BoardSize;
+                moves.Add(new Square(currentSquare.Row, col));
+                moves.Add(new Square(row, currentSquare.Col));
+            }
+
+            return moves;
         }
     }
 }
