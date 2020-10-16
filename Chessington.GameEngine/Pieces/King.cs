4662e7259d70ae02b07c3ce974be31086603e6c1
Shivam <Shivam.Chandarana@softwire.com>
2020-10-16 17:20:36 +0100

    Red-07 complete

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,19 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var moves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+            var i = 1;
+            moves.AddIfOnBoard(new Square(currentSquare.Row + i, currentSquare.Col + i));
+            moves.AddIfOnBoard(new Square(currentSquare.Row - i, currentSquare.Col + i));
+            moves.AddIfOnBoard(new Square(currentSquare.Row + i, currentSquare.Col - i));
+            moves.AddIfOnBoard(new Square(currentSquare.Row - i, currentSquare.Col - i));
+            moves.AddIfOnBoard(new Square(currentSquare.Row + i, currentSquare.Col));
+            moves.AddIfOnBoard(new Square(currentSquare.Row - i, currentSquare.Col));
+            moves.AddIfOnBoard(new Square(currentSquare.Row, currentSquare.Col + i));
+            moves.AddIfOnBoard(new Square(currentSquare.Row, currentSquare.Col - i));
+
+            return moves;
         }
     }
 }
