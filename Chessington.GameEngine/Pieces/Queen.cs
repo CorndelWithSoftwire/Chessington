93171ffcf4102b5759a9cabccb088c8f421e3701
Shivam <Shivam.Chandarana@softwire.com>
2020-10-16 15:57:07 +0100

    Red-05 Complete

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -10,7 +10,21 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var moves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+            for (int i = 1; i < GameSettings.BoardSize; i++)
+            {
+                AddIfOnBoard(new Square(currentSquare.Row + i, currentSquare.Col + i), moves);
+                AddIfOnBoard(new Square(currentSquare.Row - i, currentSquare.Col + i), moves);
+                AddIfOnBoard(new Square(currentSquare.Row + i, currentSquare.Col - i), moves);
+                AddIfOnBoard(new Square(currentSquare.Row - i, currentSquare.Col - i), moves);
+                AddIfOnBoard(new Square(currentSquare.Row + i, currentSquare.Col), moves);
+                AddIfOnBoard(new Square(currentSquare.Row - i, currentSquare.Col), moves);
+                AddIfOnBoard(new Square(currentSquare.Row, currentSquare.Col + i), moves);
+                AddIfOnBoard(new Square(currentSquare.Row, currentSquare.Col - i), moves);
+            }
+
+            return moves;
         }
     }
 }
