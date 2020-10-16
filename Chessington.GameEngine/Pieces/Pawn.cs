d20e47a324b941e4a81f942c0c69e3e7183fd124
Shivam <Shivam.Chandarana@softwire.com>
2020-10-16 13:53:40 +0100

    Test Red-01 Complete

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,7 +10,12 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var moves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+            moves.Add(this.Player == Player.White
+                ? Square.At(currentSquare.Row - 1, currentSquare.Col)
+                : Square.At(currentSquare.Row + 1, currentSquare.Col));
+            return moves;
         }
     }
 }
