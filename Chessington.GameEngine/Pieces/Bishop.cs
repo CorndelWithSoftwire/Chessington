17cec7f239896f3ae4e17670bb1549a4a8e3cefe
Daniel Barbulescu <Daniel.Barbulescu@softwire.com>
2022-07-19 13:12:13 +0100

    Made Bishop Move

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,7 +10,20 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var expectedMoves = new List<Square>();
+
+            //Checking the backwards diagonal, i.e. 0,0 1,1, 2,2
+            for (var i = 0; i < 8; i++)
+                expectedMoves.Add(Square.At(i, i));
+
+            //Checking the forwards diagonal i.e. 5,3 6,2 7,1
+            for (var i = 1; i < 8; i++)
+                expectedMoves.Add(Square.At(i, 8 - i));
+
+            //Get rid of our starting location.
+            expectedMoves.RemoveAll(s => s == Square.At(board.FindPiece(this).Row, board.FindPiece(this).Col));
+
+            return expectedMoves;
         }
     }
 }
