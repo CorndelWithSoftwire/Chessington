180f5a2dae2c93f1b9a3ebf57302cb8d2f382565
Daniel Barbulescu <Daniel.Barbulescu@softwire.com>
2022-07-19 11:32:12 +0100

    Made pawn move

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,7 +10,19 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            //determine color of player
+            int pos;
+            if (this.Player == Player.White)
+                pos = -1;
+            else pos = 1;
+
+            //create new square at new position to later add to a list with available moves
+            var new_square = Square.At(board.FindPiece(this).Row + pos, board.FindPiece(this).Col);
+            var moves = new List<Square>();
+            moves.Add((new_square));
+
+            return moves;
+            // return Enumerable.Empty<Square>();
         }
     }
 }
