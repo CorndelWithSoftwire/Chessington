202ad02166c5bbdcc162e3436aded27053514e0d
Daniel Barbulescu <Daniel.Barbulescu@softwire.com>
2022-07-19 12:49:33 +0100

    Made rook move

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,18 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var expectedMoves = new List<Square>();
+
+            for (var i = 0; i < 8; i++)
+                expectedMoves.Add(Square.At(board.FindPiece(this).Row, i));
+
+            for (var i = 0; i < 8; i++)
+                expectedMoves.Add(Square.At(i, board.FindPiece(this).Col));
+
+            //Get rid of our starting location.
+            expectedMoves.RemoveAll(s => s == Square.At(board.FindPiece(this).Row, board.FindPiece(this).Col));
+
+            return expectedMoves;
         }
     }
 }
