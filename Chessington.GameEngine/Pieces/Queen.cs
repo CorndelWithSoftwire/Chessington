b55a0806a16728e78ba92114886a79f002421e30
Daniel Barbulescu <Daniel.Barbulescu@softwire.com>
2022-07-19 14:06:29 +0100

    Made Queen Move

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -18,7 +18,50 @@ namespace Chessington.GameEngine.Pieces
             for (int i = 0; i < 8; i++)
                 expectedMoves.Add(Square.At(i, board.FindPiece(this).Col));
 
+            int current_col_pos = board.FindPiece(this).Col;
+            int current_row_pos = board.FindPiece(this).Row;
 
+            //check from current to top left corner : (can't use do while loop => need a check for first one too)
+            current_row_pos -= 1;
+            current_col_pos -= 1;
+            while (current_row_pos >= 0 && current_col_pos >= 0)
+            {
+                expectedMoves.Add(Square.At(current_row_pos, current_col_pos));
+                current_row_pos -= 1;
+                current_col_pos -= 1;
+            }
+
+            //check from current to top right corner
+            current_col_pos = board.FindPiece(this).Col;
+            current_row_pos = board.FindPiece(this).Row;
+            while (current_row_pos >= 0 && current_col_pos <= 7)
+            {
+                expectedMoves.Add(Square.At(current_row_pos, current_col_pos));
+                current_row_pos -= 1;
+                current_col_pos += 1;
+            }
+
+            //check from current to low left corner
+            current_col_pos = board.FindPiece(this).Col;
+            current_row_pos = board.FindPiece(this).Row;
+            while (current_row_pos <= 7 && current_col_pos >= 0)
+            {
+                expectedMoves.Add(Square.At(current_row_pos, current_col_pos));
+                current_row_pos += 1;
+                current_col_pos -= 1;
+            }
+
+            //check from current to low right corner
+            current_col_pos = board.FindPiece(this).Col;
+            current_row_pos = board.FindPiece(this).Row;
+            while (current_row_pos >= 0 && current_col_pos <= 7)
+            {
+                expectedMoves.Add(Square.At(current_row_pos, current_col_pos));
+                current_row_pos += 1;
+                current_col_pos += 1;
+            }
+
+            expectedMoves.RemoveAll(s => s == Square.At(board.FindPiece(this).Row, board.FindPiece(this).Col));
 
             return expectedMoves;
         }
