898806bf46809519cb5eab046d125b56048812dd
Daniel Barbulescu <Daniel.Barbulescu@softwire.com>
2022-07-20 06:31:35 +0100

    Queen can move ONLY with clear path ahead

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -12,12 +12,59 @@ namespace Chessington.GameEngine.Pieces
         {
             var expectedMoves = new List<Square>();
 
-            for (int i = 0; i < 8; i++)
+            //check clear path for rook to the left
+            for (var i = board.FindPiece(this).Col + 1; i < 8; i++)
+            {
+                if (board.isSquareOccupiedByFriend(board.FindPiece(this).Row, i))
+                    break;
+                if (board.isSquareOccupiedByEnemy(board.FindPiece(this).Row, i))
+                {
+                    expectedMoves.Add(Square.At(board.FindPiece(this).Row, i));
+                    break;
+                }
                 expectedMoves.Add(Square.At(board.FindPiece(this).Row, i));
+            }
 
-            for (int i = 0; i < 8; i++)
+            //check clear path for rook to the right
+            for (var i = board.FindPiece(this).Col - 1; i >= 0; i--)
+            {
+                if (board.isSquareOccupiedByFriend(board.FindPiece(this).Row, i))
+                    break;
+                if (board.isSquareOccupiedByEnemy(board.FindPiece(this).Row, i))
+                {
+                    expectedMoves.Add(Square.At(board.FindPiece(this).Row, i));
+                    break;
+                }
+                expectedMoves.Add(Square.At(board.FindPiece(this).Row, i));
+            }
+
+            //check clear path for rook to go down
+            for (var i = board.FindPiece(this).Row + 1; i < 8; i++)
+            {
+                if (board.isSquareOccupiedByFriend(i, board.FindPiece(this).Col))
+                    break;
+                if (board.isSquareOccupiedByEnemy(i, board.FindPiece(this).Col))
+                {
+                    expectedMoves.Add(Square.At(i, board.FindPiece(this).Col));
+                    break;
+                }
                 expectedMoves.Add(Square.At(i, board.FindPiece(this).Col));
+            }
 
+            //check clear path for rook to go down
+            for (var i = board.FindPiece(this).Row - 1; i >= 0; i--)
+            {
+                if (board.isSquareOccupiedByFriend(i, board.FindPiece(this).Col))
+                    break;
+                if (board.isSquareOccupiedByEnemy(i, board.FindPiece(this).Col))
+                {
+                    expectedMoves.Add(Square.At(i, board.FindPiece(this).Col));
+                    break;
+                }
+                expectedMoves.Add(Square.At(i, board.FindPiece(this).Col));
+            }
+
+            //diagonally check
             int current_col_pos = board.FindPiece(this).Col;
             int current_row_pos = board.FindPiece(this).Row;
 
@@ -26,6 +73,13 @@ namespace Chessington.GameEngine.Pieces
             current_col_pos -= 1;
             while (current_row_pos >= 0 && current_col_pos >= 0)
             {
+                if (board.isSquareOccupiedByFriend(current_row_pos, current_col_pos))
+                    break;
+                if (board.isSquareOccupiedByEnemy(current_row_pos, current_col_pos))
+                {
+                    expectedMoves.Add(Square.At(current_row_pos, current_col_pos));
+                    break;
+                }
                 expectedMoves.Add(Square.At(current_row_pos, current_col_pos));
                 current_row_pos -= 1;
                 current_col_pos -= 1;
@@ -34,8 +88,17 @@ namespace Chessington.GameEngine.Pieces
             //check from current to top right corner
             current_col_pos = board.FindPiece(this).Col;
             current_row_pos = board.FindPiece(this).Row;
+            current_row_pos -= 1;
+            current_col_pos += 1;
             while (current_row_pos >= 0 && current_col_pos <= 7)
             {
+                if (board.isSquareOccupiedByFriend(current_row_pos, current_col_pos))
+                    break;
+                if (board.isSquareOccupiedByEnemy(current_row_pos, current_col_pos))
+                {
+                    expectedMoves.Add(Square.At(current_row_pos, current_col_pos));
+                    break;
+                }
                 expectedMoves.Add(Square.At(current_row_pos, current_col_pos));
                 current_row_pos -= 1;
                 current_col_pos += 1;
@@ -44,8 +107,17 @@ namespace Chessington.GameEngine.Pieces
             //check from current to low left corner
             current_col_pos = board.FindPiece(this).Col;
             current_row_pos = board.FindPiece(this).Row;
+            current_row_pos += 1;
+            current_col_pos -= 1;
             while (current_row_pos <= 7 && current_col_pos >= 0)
             {
+                if (board.isSquareOccupiedByFriend(current_row_pos, current_col_pos))
+                    break;
+                if (board.isSquareOccupiedByEnemy(current_row_pos, current_col_pos))
+                {
+                    expectedMoves.Add(Square.At(current_row_pos, current_col_pos));
+                    break;
+                }
                 expectedMoves.Add(Square.At(current_row_pos, current_col_pos));
                 current_row_pos += 1;
                 current_col_pos -= 1;
@@ -54,8 +126,17 @@ namespace Chessington.GameEngine.Pieces
             //check from current to low right corner
             current_col_pos = board.FindPiece(this).Col;
             current_row_pos = board.FindPiece(this).Row;
+            current_row_pos += 1;
+            current_col_pos += 1;
             while (current_row_pos >= 0 && current_col_pos <= 7)
             {
+                if (board.isSquareOccupiedByFriend(current_row_pos, current_col_pos))
+                    break;
+                if (board.isSquareOccupiedByEnemy(current_row_pos, current_col_pos))
+                {
+                    expectedMoves.Add(Square.At(current_row_pos, current_col_pos));
+                    break;
+                }
                 expectedMoves.Add(Square.At(current_row_pos, current_col_pos));
                 current_row_pos += 1;
                 current_col_pos += 1;
