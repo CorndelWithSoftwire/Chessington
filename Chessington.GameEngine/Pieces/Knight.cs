e8ee9efcbf511589c8d51413820be6da12b3eebd
Daniel Barbulescu <Daniel.Barbulescu@softwire.com>
2022-07-20 06:46:33 +0100

    Knights can't take friendly pieces anymore

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -12,28 +12,28 @@ namespace Chessington.GameEngine.Pieces
         {
             var expectedMoves = new List<Square>();
 
-            if(board.FindPiece(this).Row - 2 >=0 && board.FindPiece(this).Col - 1 >= 0)
+            if(board.FindPiece(this).Row - 2 >=0 && board.FindPiece(this).Col - 1 >= 0 && !board.isSquareOccupiedByFriend(board.FindPiece(this).Row - 2,board.FindPiece(this).Col - 1))
                 expectedMoves.Add(Square.At(board.FindPiece(this).Row - 2, board.FindPiece(this).Col - 1));
 
-            if (board.FindPiece(this).Row - 1 >= 0 && board.FindPiece(this).Col - 2 >= 0)
+            if (board.FindPiece(this).Row - 1 >= 0 && board.FindPiece(this).Col - 2 >= 0 && !board.isSquareOccupiedByFriend(board.FindPiece(this).Row - 1, board.FindPiece(this).Col - 2))
                 expectedMoves.Add(Square.At(board.FindPiece(this).Row - 1, board.FindPiece(this).Col - 2));
 
-            if (board.FindPiece(this).Row - 2 >= 0 && board.FindPiece(this).Col + 1 <= 7)
+            if (board.FindPiece(this).Row - 2 >= 0 && board.FindPiece(this).Col + 1 <= 7 && !board.isSquareOccupiedByFriend(board.FindPiece(this).Row - 2, board.FindPiece(this).Col + 1))
                 expectedMoves.Add(Square.At(board.FindPiece(this).Row - 2, board.FindPiece(this).Col + 1));
 
-            if (board.FindPiece(this).Row - 1 >= 0 && board.FindPiece(this).Col + 2 <= 7)
+            if (board.FindPiece(this).Row - 1 >= 0 && board.FindPiece(this).Col + 2 <= 7 && !board.isSquareOccupiedByFriend(board.FindPiece(this).Row - 1, board.FindPiece(this).Col + 2))
                 expectedMoves.Add(Square.At(board.FindPiece(this).Row - 1, board.FindPiece(this).Col + 2));
 
-            if (board.FindPiece(this).Row + 2 <= 7 && board.FindPiece(this).Col - 1 >= 0)
+            if (board.FindPiece(this).Row + 2 <= 7 && board.FindPiece(this).Col - 1 >= 0 && !board.isSquareOccupiedByFriend(board.FindPiece(this).Row + 2, board.FindPiece(this).Col - 1))
                 expectedMoves.Add(Square.At(board.FindPiece(this).Row + 2, board.FindPiece(this).Col - 1));
 
-            if (board.FindPiece(this).Row + 1 <= 7 && board.FindPiece(this).Col - 2 >= 0)
+            if (board.FindPiece(this).Row + 1 <= 7 && board.FindPiece(this).Col - 2 >= 0 && !board.isSquareOccupiedByFriend(board.FindPiece(this).Row + 1, board.FindPiece(this).Col - 2))
                 expectedMoves.Add(Square.At(board.FindPiece(this).Row + 1, board.FindPiece(this).Col - 2));
 
-            if (board.FindPiece(this).Row + 2 <= 7 && board.FindPiece(this).Col + 1 <= 7)
+            if (board.FindPiece(this).Row + 2 <= 7 && board.FindPiece(this).Col + 1 <= 7 && !board.isSquareOccupiedByFriend(board.FindPiece(this).Row + 2, board.FindPiece(this).Col + 1))
                 expectedMoves.Add(Square.At(board.FindPiece(this).Row + 2, board.FindPiece(this).Col + 1));
 
-            if (board.FindPiece(this).Row + 1 <= 7 && board.FindPiece(this).Col + 2 <= 7)
+            if (board.FindPiece(this).Row + 1 <= 7 && board.FindPiece(this).Col + 2 <= 7 && !board.isSquareOccupiedByFriend(board.FindPiece(this).Row + 1, board.FindPiece(this).Col + 2))
                 expectedMoves.Add(Square.At(board.FindPiece(this).Row + 1, board.FindPiece(this).Col + 2));
 
             return expectedMoves;
